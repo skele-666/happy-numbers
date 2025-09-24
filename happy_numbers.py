3fe7ed3b4ab4d7198462d020d8bc423da5f90665
Ella Jozinovic <ellajozi01@gmail.com>
2025-09-24 13:42:44 +0930

    Add logic to detect non-happy numbers.

--- happy_numbers.py
+++ happy_numbers.py
@@ -1,20 +1,21 @@
-# A happy number is defined by the following process. Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers. Display an example of your output here. Find first 8 happy numbers.
-
 # Happy number function
 def is_happy_num(n):
+    seen = set()
+
     while n != 1:
+        if n in seen:
+            return False
+        seen.add(n)
+
         str_n = str(n)
         str_arr = list(str_n)
-        
-        num_arr = [int(n) for n in str_arr]
-        print(num_arr)
 
-        sqr_num_arr = [n ** 2 for n in num_arr]
-        print(sqr_num_arr)
-        
+        num_arr = [int(i) for i in str_arr]
+
+        sqr_num_arr = [n**2 for n in num_arr]
+
         n = sum(sqr_num_arr)
-        
-        print(n)
+    return True
 
 
 # Get user input
@@ -33,5 +34,6 @@ while True:
     else:
         break
 
-print(n)
-is_happy_num(n)
+# Run happy number function
+print(f"Is {n} a happy number?")
+print("Yes!" if is_happy_num(n) else "No.")
